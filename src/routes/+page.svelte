290c4cb89f05fd368300ae090403e91f06552012
SamDev-7 <hello@samliu.dev>
2022-11-05 19:47:16 -0400

    Add date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script>
-    let time, apm;
+    let time, apm, day;
 
     function getTime() {
         let date = new Date();
-        time = (date.getHours() >= 12 ? date.getHours() - 12 : date.getHours())  + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
+        time = (date.getHours() > 12 ? date.getHours() - 12 : date.getHours())  + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
         apm = date.getHours() >= 12 ? "PM" : "AM";
+        day = date.toLocaleDateString(undefined, {
+            weekday: "long",
+            month: "long",
+            day: "numeric",
+        });
     }
 
     setInterval(getTime, 1000);
@@ -14,4 +19,5 @@
 
 <div class="text-center my-20">
     <h1 class="text-9xl font-semibold tracking-wide">{time}<span class="text-5xl text-blue-300">{apm}</span></h1>
+    <h2 class="text-blue-300 uppercase text-3xl font-semibold tracking-wide">{day}</h2>
 </div>
