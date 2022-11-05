2a7a78d24750fd39463c6f0dcf0c8246fad972b2
SamDev-7 <hello@samliu.dev>
2022-11-05 19:50:22 -0400

    Remove date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script>
-    let time, apm, day;
+    let time, apm;
 
     function getTime() {
         let date = new Date();
         time = (date.getHours() > 12 ? date.getHours() - 12 : date.getHours())  + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
         apm = date.getHours() >= 12 ? "PM" : "AM";
-        day = date.toLocaleDateString(undefined, {
-            weekday: "long",
-            month: "long",
-            day: "numeric",
-        });
     }
 
     setInterval(getTime, 1000);
@@ -19,5 +14,4 @@
 
 <div class="text-center my-20">
     <h1 class="text-9xl font-semibold tracking-wide">{time}<span class="text-5xl text-blue-300">{apm}</span></h1>
-    <h2 class="text-blue-300 uppercase text-3xl font-semibold tracking-wide">{day}</h2>
 </div>
