a2c7a8a3aaa948c4b42eb6639589d8c9dcfc1a6e
SamDev-7 <hello@samliu.dev>
2022-11-13 22:41:01 -0500

    Fix import

--- src/routes/api/light/switch/+server.js
+++ src/routes/api/light/switch/+server.js
@@ -1,10 +1,12 @@
-import { python } from 'pythonia'
+import { python } from 'pythonia';
+import { json } from '@sveltejs/kit';
+
 
 const gpio = await python('RPi.GPIO');
 
 await gpio.setmode(await gpio.BCM);;
 
-await gpio.setup$(20, await gpio.OUT, {initial: await gpio.HIGH})
+await gpio.setup$(20, await gpio.OUT, {initial: await gpio.HIGH});
 await gpio.setup(21, await gpio.IN);
 
 export async function GET({ url }) {
